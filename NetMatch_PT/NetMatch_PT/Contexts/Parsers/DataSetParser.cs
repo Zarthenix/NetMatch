7321f3a1185e42f3def28efcb51305113978157c
vince <vince.heesters@student.fontys.nl>
2020-03-11 15:41:00 +0100

    initial dinges

---
+++ NetMatch_PT/NetMatch_PT/Contexts/Parsers/DataSetParser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NetMatch_PT.Models;
+using System.Data;
+
+namespace NetMatch_PT.Contexts.Parsers
+{
+    public class DataSetParser
+    {
+        public static Accommodation DataSetToAccommodation(DataSet set, int rowIndex)
+        {
+            return new Accommodation((int)set.Tables[0].Rows[rowIndex][0])
+            {
+                
+                Title = (string)set.Tables[0].Rows[rowIndex][1],
+                Description = (string)set.Tables[0].Rows[rowIndex][2],
+                Price = (decimal)set.Tables[0].Rows[rowIndex][3],
+                Image = (byte[])set.Tables[0].Rows[rowIndex][4]
+            };
+        }
+    }
+}
--- NetMatch_PT/NetMatch_PT/Models/Accommodation.cs
+++ NetMatch_PT/NetMatch_PT/Models/Accommodation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NetMatch_PT.Models.Enums;
 
 namespace NetMatch_PT.Models
 {
@@ -10,19 +11,24 @@ namespace NetMatch_PT.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public DateTime Date { get; set; }
         public decimal Price { get; set; }
         public byte[] Image { get; set; }
+        public TravelTypes Traveltype { get; set; }
+        public Countries Countries { get; set; }
+        public List<AccommodationDate> DateList { get; set; }
 
-        public Accommodation(int id, string title, DateTime date,
+        public Accommodation(int id, string title,
             decimal price, byte[] image, string description)
         {
             Id = id;
             Title = title;
-            Date = date;
             Price = price;
             Image = image;
             Description = description;
+        }
+        public Accommodation(int id)
+        {
+            Id = id;
         }
     }
 
