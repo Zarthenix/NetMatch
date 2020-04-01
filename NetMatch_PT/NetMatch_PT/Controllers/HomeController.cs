46f7e96303cb24f739bacc9ce1a9d41d175cdf15
trtnt <42801247+trtnt@users.noreply.github.com>
2020-04-01 14:19:53 +0200

    Functionaliteit PDF boeken

--- NetMatch_PT/NetMatch_PT/Controllers/HomeController.cs
+++ NetMatch_PT/NetMatch_PT/Controllers/HomeController.cs
@@ -47,7 +47,12 @@ namespace NetMatch_PT.Controllers
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult PDFboeking()
+        {
+            return View();
+        }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
